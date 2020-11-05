c905590ec043a77d3e678b2102cb0861e7d925d9
n1ckDotEXE <admin@nicholasnam.com>
2020-11-05 11:58:02 -0500

    Switch and encode-work

--- encode-word.js
+++ encode-word.js
@@ -1,14 +1,33 @@
 const emojis = require('./emojis.js');
 
 
-// string-building version
+// // string-building version
 const encodeWord = function(word) {
     let result = '';
     for (const char of word) {
         for (const emoji of emojis) {
-            if (emoji.letter === char) {
+            if (emoji.letter === char.toLowerCase()) {
                 result += emoji.symbol;
+            } else {
+                result += char;
             }
         }
     }
+    return result;
 }
+
+module.export = encodeWord;
+
+// module.exports =  encodeWord = word => {
+//     let result = '';
+//     for(i in word) {
+//         for(j in emojis) {
+//             if(word[i].toLowerCase() === emojis[j].letter) {
+//                 result += emojis[j].symbol;
+//             } else {
+//                 result += word[i];
+//             }
+//         }
+//     }
+//     return result;
+// }
--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 const translateWord = require('./translate-word.js');
 const encodeWord = require('./encode-word.js');
 
-
-const words = process.argv.slice(2);
+// const userInput = 
+// const words = process.argv.slice(2);
+// const feature = words[0];
+// // const encoding = words.map(encodeWord);
+// // console.log(encoding.join(' '));
+// const translation = words.map(translateWord)
 // const encoding = words.map(encodeWord);
-// console.log(encoding.join(' '));
-const translation = words.map(translateWord)
-console.log(translation.join(' '));
+
+// console.log(translation.join(' '));
+
+const command = process.argv[2];
+const words = process.argv.slice(3);
+
+const translation = words.map(translateWord);
+const encoding = words.map(encodeWord);
+
+switch(command) {
+    case 'translate':
+        console.log(translation.join(' '));
+        break;
+    case 'encode':
+        console.log(encoding.join(' '));
+        break;
+    default:
+        console.log('Please enter a valid argument (translate or encode)');    
+    }
