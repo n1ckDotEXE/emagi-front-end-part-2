b261226b2eb68ebc491e6f24f69ff70a3b5803c3
n1ckDotEXE <admin@nicholasnam.com>
2020-11-05 12:03:02 -0500

    Code cleanup of template

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
 const translateWord = require('./translate-word.js');
 const encodeWord = require('./encode-word.js');
 
-// const userInput = 
-// const words = process.argv.slice(2);
-// const feature = words[0];
-// // const encoding = words.map(encodeWord);
-// // console.log(encoding.join(' '));
-// const translation = words.map(translateWord)
-// const encoding = words.map(encodeWord);
-
-// console.log(translation.join(' '));
-
 const command = process.argv[2];
 const words = process.argv.slice(3);
 
@@ -26,4 +16,4 @@ switch(command) {
         break;
     default:
         console.log('Please enter a valid argument (translate or encode)');    
-    }
+}
